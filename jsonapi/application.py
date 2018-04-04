9a2b4cf879866f3623f117e97a4c45ed3bb2579e
Mark Jacobson <35484465+mark-saeon@users.noreply.github.com>
2018-04-04 12:16:51 +0200

    Add jsonContent API for listing institutions

--- jsonapi/application.py
+++ jsonapi/application.py
@@ -2,11 +2,27 @@
 import cherrypy
 import re
 
-from jsonapi import CONFIG_FILE, auth
+from jsonapi import CONFIG_FILE
 from ckanapi import RemoteCKAN
 
 
 class Application:
+
+    @staticmethod
+    def _set_response_headers():
+        cherrypy.response.headers['Access-Control-Allow-Origin'] = '*'
+        cherrypy.response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
+        cherrypy.response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+
+    @staticmethod
+    def _authenticate(data_dict):
+        # TODO: authenticate with identity server and return access token
+        username = data_dict.pop('__ac_name', '')
+        password = data_dict.pop('__ac_password', '')
+        apikey = cherrypy.config.get('ckan.apikey')
+        if not apikey:
+            raise cherrypy.HTTPError(403, 'Access denied')
+        return apikey
 
     @staticmethod
     def _extract_error(e):
@@ -24,44 +40,70 @@ class Application:
     @cherrypy.tools.json_in()
     @cherrypy.tools.json_out()
     def create_metadata(self, institution, repository):
-        cherrypy.response.headers['Access-Control-Allow-Origin'] = '*'
-        cherrypy.response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
-        cherrypy.response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        self._set_response_headers()
+        if cherrypy.request.method != 'POST':
+            return
 
-        if cherrypy.request.method == 'POST':
-            data = cherrypy.request.json
-            username = data.pop('__ac_name', '')
-            password = data.pop('__ac_password', '')
-            schema_name = data.pop('metadataType', '')
-            metadata_json = data.pop('jsonData', '')
+        ckanurl = cherrypy.config['ckan.url']
+        data = cherrypy.request.json
+        apikey = self._authenticate(data)
 
-            ckanurl = cherrypy.config['ckan.url']
-            apikey = auth.authenticate(username, password)
-            data_dict = {
-                'owner_org': institution,
-                'metadata_collection_id': repository,
-                'infrastructures': [],
-                'schema_name': schema_name,
-                'schema_version': '',
-                'content_json': metadata_json,
-                'content_raw': '',
-                'content_url': '',
+        schema_name = data.pop('metadataType', '')
+        metadata_json = data.pop('jsonData', '')
+
+        try:
+            with RemoteCKAN(ckanurl, apikey=apikey) as ckan:
+                ckanresult = ckan.call_action('metadata_record_create', data_dict={
+                    'owner_org': institution,
+                    'metadata_collection_id': repository,
+                    'infrastructures': [],
+                    'schema_name': schema_name,
+                    'schema_version': '',
+                    'content_json': metadata_json,
+                    'content_raw': '',
+                    'content_url': '',
+                })
+            return {
+                'status': 'success',
+                'token': ckanresult['name'],
+                'url': ckanurl + '/api/action/metadata_record_show?id=' + ckanresult['id'],
+                'uid': ckanresult['id'],
+                'doi': '',
             }
-            try:
-                with RemoteCKAN(ckanurl, apikey=apikey) as ckan:
-                    ckanresult = ckan.call_action('metadata_record_create', data_dict)
-                return {
-                    'status': 'success',
-                    'token': ckanresult['name'],
-                    'url': ckanurl + '/api/action/metadata_record_show?id=' + ckanresult['id'],
-                    'uid': ckanresult['id'],
-                    'doi': '',
-                }
-            except Exception as e:
-                return {
-                    'status': 'failed',
-                    'msg': self._extract_error(e),
-                }
+        except Exception as e:
+            return {
+                'status': 'failed',
+                'msg': self._extract_error(e),
+            }
+
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
+    def list_institutions(self):
+        self._set_response_headers()
+        if cherrypy.request.method != 'POST':
+            return
+
+        ckanurl = cherrypy.config['ckan.url']
+        data = cherrypy.request.json
+        apikey = self._authenticate(data)
+
+        types = data.pop('types', '')
+        if types != 'Institution':
+            return {
+                'status': 'failed',
+                'msg': "Missing parameter 'types'",
+            }
+
+        try:
+            with RemoteCKAN(ckanurl, apikey=apikey) as ckan:
+                return ckan.call_action('organization_list', data_dict={'all_fields': True})
+
+        except Exception as e:
+            return {
+                'status': 'failed',
+                'msg': self._extract_error(e),
+            }
 
 
 if __name__ == "__main__":
@@ -72,6 +114,13 @@ if __name__ == "__main__":
         route='/Institutions/{institution}/{repository}/metadata/jsonCreateMetadataAsJson',
         controller=application,
         action='create_metadata',
+        conditions=dict(method=['OPTIONS', 'POST']),
+    )
+    dispatcher.connect(
+        name='list-institutions',
+        route='/Institutions/jsonContent',
+        controller=application,
+        action='list_institutions',
         conditions=dict(method=['OPTIONS', 'POST']),
     )
     cherrypy.config.update(CONFIG_FILE)
